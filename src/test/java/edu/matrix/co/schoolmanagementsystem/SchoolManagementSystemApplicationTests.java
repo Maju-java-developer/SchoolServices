fe7444f94a847003ccebcc6812fde24f86f26291
Majid Hussain <122609422+Maju-java-developer@users.noreply.github.com>
2025-09-29 15:43:36 +0500

    Initial commit

---
+++ src/test/java/edu/matrix/co/schoolmanagementsystem/SchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.matrix.co.schoolmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
